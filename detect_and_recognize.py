d785c1f5bcc0629f9e8054db629ded8e1d46e9e8
hafizeogut <hafizecemileogut@gmail.com>
2024-07-26 17:06:12 +0300

    detect_and_recognize

--- detect_and_recognize.py
+++ detect_and_recognize.py
@@ -14,8 +14,8 @@ color = (255,255,0)
 def detect_sunflower(image,model,display = False):
     start = time.time()#işlemin başlangıç zamanını al
     detections = model.predict(image)[0].boxes.data #Model ile tahmin yap ve tespit edilen kutuları al
-    print(detections) #Tespit edilen kutuları yazdır
-    print("shape",detections.shape)#Tespit edilen kutuları yazdır
+    # print(detections) #Tespit edilen kutuları yazdır
+    # print("shape",detections.shape)#Tespit edilen kutuları yazdır
 
     #Eğer tespit edilen kutular boş değilse
     if detections.shape != torch.Size([0,6]):
@@ -27,36 +27,50 @@ def detect_sunflower(image,model,display = False):
                 continue
             boxes.append(detection[:4])
             confidences.append(detection[4])
+        # print("confidences",confidences)
         print(f"{len(boxes)} Number sunflower have been detected")
         sunflower_list = []
 
-        print(boxes)
+        # print(boxes)
         for i in range (len(boxes)):
             xmin, ymin, xmax, ymax = int(boxes[i][0]),int(boxes[i][1]),int(boxes[i][2]),int(boxes[i][3])
-            sunflower_list.append([xmin, ymin, xmax, ymax])
-            cv2.rectangle(image,(xmin,ymin),color,2)
-            text = "Sunflower: {:.2f}%".format(confidences[i]* 100,cv2.FONT_HERSHEY_SIMPLEX,0.5,color,2)
-            cv2.putText(image,text,(xmin,ymin -5))
-        if display:
-            sunflower = image[ymin:ymax, xmin:xmax]
-            cv2.imshow(f"Number plate",sunflower)
-        end = time.time()
-        print(f"Time to detect the sunflower: {end-start}*1000: .0f milliseconds")
+            sunflower_list.append([[xmin, ymin, xmax, ymax],"ergergerg"])
 
+            cv2.rectangle(image,(xmin,ymin),(xmax,ymax),color,2)
+            text = "Sunflower: {:.2f}%".format(confidences[i]* 100)
+            cv2.putText(image,text,(xmin,ymin - 5),cv2.FONT_HERSHEY_SIMPLEX,1,color,2)
+
+            if display:
+                sunflower = image[ymin:ymax, xmin:xmax]
+                cv2.imshow(f"Number plate {i}",sunflower)
+
+        end = time.time()
+        print(f"Time to detect the sunflower: {end-  start}*1000: .0f milliseconds")
         return sunflower_list
 
     else:
         print("No number plate have been detected")
         return [ ]
     
-if __name__ == "__main__"
+
+if __name__ == "__main__":
     #YOLO modelini yükle
     model = YOLO("/home/hafizeogut/Desktop/sunflower_object_detection/runs/detect/yolov8n_sunflower/weights/best.pt")
-
     #Test görüntüsünün yolu
-    image = "/home/hafizeogut/Desktop/sunflower_object_detection/sunflower/images/test/00af3df40b_jpg.rf.4296f31f894c60a792281b03f25c6145.jpg"
+    file_path = "/home/hafizeogut/Desktop/test.jpg"
+    _, file_extension = os.path.splitext(file_path)
+    # print("file_extension",file_extension)
 
-    detect_sunflower(image,model)
+    if file_extension.lower() in [".jpg",".jpeg",".png"]:
+        print( "Proprocessing the image...")
+        image = cv2.imread(file_path)
+        sunflower_list = detect_sunflower(image,model,display=True)
+        cv2.imshow("image",image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    elif file_extension in [".mp4",".mkv",".avi",".wmv",".mov"] :
+        pass
+
 """
 print(detections)
          x1         ,y1         ,x2         ,y2        ,confidence:Model nesneyi tanımlma güven aralığı  ,class:Tespit edilen nesnenin sınıf
